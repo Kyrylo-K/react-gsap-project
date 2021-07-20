7ddfeef9e761cbc6869509214926379e6ac6e285
Kyrylo-K <kordiukovkyrylo@outlook.com>
2021-07-19 21:15:20 -0700

    aaawefwfe

---
+++ build/precache-manifest.55a6f79860f735d18e2e58cd16ea6011.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "699b12af93a15380ffd1",
+    "url": "/static/css/main.ab4eb67d.chunk.css"
+  },
+  {
+    "revision": "699b12af93a15380ffd1",
+    "url": "/static/js/main.699b12af.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "f9c2cc69a4ff2a25cba9",
+    "url": "/static/js/2.f9c2cc69.chunk.js"
+  },
+  {
+    "revision": "29c10c455a6e9d0b73c04f054a5c4c99",
+    "url": "/index.html"
+  }
+];
